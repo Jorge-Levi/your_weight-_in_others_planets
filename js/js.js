d42c191484e54632b3f6f5dbf40c41cd0232af67
Jorge Levi <tapialugardo29@gmail.com>
2021-01-21 13:26:50 -0600

    Add interactivity

--- js/js.js
+++ js/js.js
@@ -1,10 +1,14 @@
 var g_tierra = 9.8;
 var g_marte = 3.7;
 var g_jupiter = 24.8;
-var peso_tierra = 77;
-var peso_marte = parseInt((peso_tierra * g_marte)/g_tierra);
-var peso_jupiter = parseInt((peso_tierra * g_jupiter)/g_tierra);
 
-document.writeln("<p>Tu peso en la tierra es de = " + peso_tierra + "</p>");
-document.writeln("<p>Tu peso en la marte es de = " + peso_marte + "</p>");
-document.writeln("<p>Tu peso en la jupiter es de = " + peso_jupiter + "</p>");
+do{
+    var peso_tierra = parseFloat(prompt("Peso en la Tierra = "));
+}while(peso_tierra < 0 || isNaN(peso_tierra));
+
+var peso_marte = (peso_tierra * g_marte)/g_tierra;
+var peso_jupiter = (peso_tierra * g_jupiter)/g_tierra;
+
+document.writeln("<p>Tu peso en la tierra es de = " + peso_tierra + " Kg</p>");
+document.writeln("<p>Tu peso en la marte es de = " + peso_marte.toFixed(2) + " Kg</p>");
+document.writeln("<p>Tu peso en la jupiter es de = " + peso_jupiter.toFixed(2) + " Kg</p>");
